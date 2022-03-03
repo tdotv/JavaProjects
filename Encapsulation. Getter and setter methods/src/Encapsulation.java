4b638fd9fd7fe080b7e548fcc1c8bd7767176cb4
tdotv <o.turelskiy@mail.ru>
2022-03-04 01:23:54 +0300

    encaps + getter and setter

---
+++ Encapsulation. Getter and setter methods/src/Encapsulation.java
@@ -0,0 +1,24 @@
+ public class Encapsulation {
+        private int a=5, b=7;
+
+        public int showResult(){
+            return a+b;
+        }
+
+     public int getA() { //получить а
+         return a;
+     }
+
+     public void setA(int a) {//установить а
+         this.a = a;
+     }
+
+     public int getB() {
+         return b;
+     }
+
+     public void setB(int b) {
+         this.b = b;
+     }
+ }
+
---
+++ Encapsulation. Getter and setter methods/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Encapsulation encapsulation = new Encapsulation();
+        encapsulation.setA(10);
+        System.out.println(encapsulation.showResult());
+    }
+}
